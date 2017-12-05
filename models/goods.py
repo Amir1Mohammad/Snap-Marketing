88bb62230191599abdf9a9738468f1eed61b4df2
Amir_Mohammad <amir_day1374@yahoo.com>
2017-12-05 23:44:38 +0330

    remove enum

--- models/goods.py
+++ models/goods.py
@@ -12,6 +12,6 @@ class Good(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), nullable=False)
-    category = db.Column(db.Enum('Dairy', 'Junk', 'Food'), nullable=False)
+    category = db.Column(db.String(25), nullable=False)
     property = db.Column(db.String(25), nullable=False)
     cost = db.Column(db.Integer, nullable=False)
